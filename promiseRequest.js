e2f15a88989848ca0e0346bd63b7563c731b5b18
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-09-07 19:47:28 +1000

    feat: successul data retrieve trough Promise

--- promiseRequest.js
+++ promiseRequest.js
@@ -6,14 +6,17 @@ function getRandomPokemon(){
 	//let result = fetch("https://pokeapi.co/api/v2/pokemon/25").then()
 	// let ID = 25;
 	// let result = fetch("https://pokeapi.co/api/v2/pokemon/" + ID).then()
-
 	let ID = 25;
 	let result = fetch(API_BASE_URL + ID).then((response) => {
-		console.log(response);
+		console.log("Response is: " + JSON.stringify(response, null, 4));
 
-		return response;
+		return response.json();
 	})
-	.then(response => response.json()).catch(error => {
+	.then(data => {
+		console.log("Data is: " + data);
+
+		return data;
+	}).catch(error => {
 		console.log(error);
 	});
 
